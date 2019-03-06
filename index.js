38eea137e947fd0e43897220679745dfbbc4929c
ElizavetaGv <geand99@yandex.ru>
2019-03-06 17:19:14 +0700

    Добавлена информация о себе

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//вопрос
+
 document.getElementById('question-save').addEventListener('click', function (){
 	var title=document.getElementById('question-title').value;
 	var question={'title': title};
@@ -20,4 +22,30 @@ function onSaveComplete(err) {
 function onLoad(snapshot) {
 	var question=snapshot.val();
 	document.getElementById('question-title').value=question.title
+}
+
+//о себе
+
+document.getElementById('myself-save').addEventListener('click', function () {
+	var myselfTitle=document.getElementById('myself-title').value;
+	var myself={'about': myselfTitle};
+	firebase.database().ref('users/test/profile').set(myself, onSaveCompleteMyself);
+	document.getElementById('loader').setAttribute('style', 'display:block;');
+});
+firebase.database().ref('users/test/profile').on('value', onLoadMyself);
+
+function onSaveCompleteMyself(err) {
+	if (err) {
+		document.getElementById('message').innerText='Ошибка при сохранении!'
+	}
+	else {
+		document.getElementById('message').innerText='Информация сохранена!'
+	}
+	document.getElementById('loader').setAttribute('style', 'display:none;');
+}
+
+function onLoadMyself(snapshot) {
+	var myself=snapshot.val();
+	if (myself!=null) {
+	document.getElementById('myself-title').value=myself.about}
 }
